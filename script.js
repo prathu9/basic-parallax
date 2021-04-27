0814861562cb9c6b3e8b26bf8f09caecf87b9c2a
prathamesh <prathameshpgs@gmail.com>
2021-04-28 00:46:45 +0530

    removed console.log

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const loadingHeader = document.querySelector(".loading-header");
-        console.log(document.querySelector(".loading-header"));
         const loading = () => {
             loadingHeader.style.display = "none";
 };
